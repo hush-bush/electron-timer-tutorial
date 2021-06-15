06a33b34c125ac71a675a873d5abde309344525b
Валуев Илья <valuevilyas2000@gmail.com>
2021-06-15 19:49:57 +0300

    second commit

---
+++ app/main_window.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+
+const { BrowserWindow } = electron;
+
+class MainWindow extends BrowserWindow {
+    constructor(url) {
+        super({
+            width: 300,
+            height: 500,
+            frame: false,
+            resizable: false,
+            show: false,
+            webPreferences: {
+            backgroundThrottling: false,
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+            }
+        });
+        this.loadURL(url);
+        this.on('blur', this.onBlur.bind(this));
+    }
+    onBlur() {
+        this.hide();
+    }
+}
+
+module.exports = MainWindow;
